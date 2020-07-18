ac403ba7fde761c47278ab4a3db9a0a0d7073cfb
Nickchen Nick <nickchen120235@gmail.com>
2020-07-18 15:00:32 +0800

    connection to mongodb is ready

--- src/api.js
+++ src/api.js
@@ -1,7 +1,12 @@
+//express initialization
 const express = require('express')
 const app = express()
 app.listen(3000, () => console.log('listening on 3000'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mc = require('mongodb').MongoClient
+mc.connect('mongodb://192.168.1.109:27017', {useUnifiedTopology: true})
+  .then(db => {
+    console.log('DB OK')
+    db.db('testdb').createCollection('testCol').then(col => console.log('collection OK'))
+  })
+  .catch(err => console.error(err))
